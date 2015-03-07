a0666fe70d5a10d43fc7711cbdc5ee74ec188e8a
caixw <xnotepad@gmail.com>
2015-03-07 22:38:44 +0800

    删除stores/doc.go文件

--- stores/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2015 by caixw, All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
-
-// 对github.com/issue9/session包的扩展，实现几个常用的session.Store。
-package stores
-
-const Version = "0.1.1.150306"
